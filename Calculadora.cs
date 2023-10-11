1bd101999ddc348452bed00f5236a2e689253a00
Zwiicker <hickzwicker@gmail.com>
2023-10-11 00:22:43 -0300

    Refatoração do código  e Projeto Concluído

--- Calculadora.cs
+++ Calculadora.cs
@@ -6,26 +6,43 @@ namespace dotnet_newtalents
 {
     public class Calculadora
     {
+
+        private List<String> listahistorico;
+        private string data;
+        public Calculadora(string data) { 
+        
+            listahistorico= new List<String>();
+            this.data = data;
+        }
         public int somar(int val1, int val2)
         {
-            return val1 + val2;
+            int res = val1 + val2;
+            listahistorico.Insert(0, "Res: " + res + " - data: " + data);  
+            return res;
         }
         public int subtrair(int val1, int val2)
         {
-            return val1 - val2;
+            int res = val1 - val2;
+            listahistorico.Insert(0, "Res: " + res + " - data: " + data);
+            return res;
         }
         public int multiplicar(int val1, int val2)
         {
-            return val1 * val2;
+            int res = val1 * val2;
+            listahistorico.Insert(0, "Res: " + res + " - data: " + data);
+            return res;
         }
         public int dividir(int val1, int val2)
         {
-            return val1 / val2;
+            int res = val1 / val2;
+            listahistorico.Insert(0, "Res: " + res + " - data: " + data);
+            return res;
         }
 
         public List<string> historico()
         {
-            return new List<string>();
+            listahistorico.RemoveRange(3, listahistorico.Count - 3);
+            return listahistorico;
         }
     }
 }
