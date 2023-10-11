09a71764c804d29f5a28ae070ebda9ea3661fe13
Zwiicker <hickzwicker@gmail.com>
2023-10-10 23:31:47 -0300

    Criado as funções da calculadora

--- Calculadora.cs
+++ Calculadora.cs
@@ -6,6 +6,26 @@ namespace dotnet_newtalents
 {
     public class Calculadora
     {
+        public int somar ( int val1, int val2)
+        {
+            return 0;
+        }
+        public int subtrair(int val1, int val2)
+        {
+            return 0;
+        }
+        public int multiplicar(int val1, int val2)
+        {
+            return 0;
+        }
+        public int dividir(int val1, int val2)
+        {
+            return 0;
+        }
 
+        public List<string> historico()
+        {
+            return new List<string>();
+        }
     }
 }
