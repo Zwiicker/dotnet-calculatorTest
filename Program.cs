7c0e00386a56db39cddeea5b866aa331885f5c92
Zwiicker <hickzwicker@gmail.com>
2023-10-10 23:15:00 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_newtalents
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
